07865ee89e3812f4133c25df2d2b46675b8d7c2c
Takahiko Wada <wada.takahiko@gmail.com>
2023-10-12 16:22:16 +0400

    fix SSG

--- src/app/calendars/1979/page.tsx
+++
@@ -1,5 +0,0 @@
-import { HundredYearCalendar } from "@/app/components/HundredYearCalendar";
-
-export default function Home() {
-  return <HundredYearCalendar startYear={1979} />;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
-const years = Array(10)
+const years = Array(71)
   .fill(0)
-  .map((_, i) => i + 1975);
+  .map((_, i) => i + 1960);
 export default function Home() {
   return (
     <div className="m-4">
       {years.map((year) => (
-        <div className="m-2" key={year}>
+        <div className="my-3 mx-5" key={year}>
           <Link
             href={`/calendars/${year}`}
             className="underline"
-          >{`${year}年生まれの100年カレンダー`}</Link>
+          >{`📅 ${year}年生まれの100年カレンダー`}</Link>
         </div>
       ))}
     </div>
